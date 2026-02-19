17098b05c10103ed30aedb2d3e435ee6f10184e6
Calvin Bagby <cbagby33@gmail.com>
2026-02-19 00:41:20 -0500

    added calculator component

---
+++ src/component/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Calculator extends React.Component{
+	constructor (props){
+		super(props)
+	}
+	render(){
+		return(
+			<div id="calculator"></div>
+		)
+	}
+}
+
+export default Calculator
