b5bf8bc8f74ae211a0b9bb2799198595d832f9c1
Calvin Bagby <cbagby33@gmail.com>
2026-02-23 03:05:00 -0500

    created buttonMap to iterate over and push button components to the calculator

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,39 +1,39 @@
 import Button from './Button'
-import data from '../buttonMap.json'
+import buttonMap from '../buttonMap.json'
 
 function Buttons() {
+	// iterate over button map to create calculator buttons
+	const buttons = buttonMap.buttons.map(button =>	
+		<Button id={button.id} label={button.label} type={button.type} />
+	);
+	// create rows for grid styling of buttons
+	const rows = buttonMap.rows.map((row, idx) => {
+		if(idx < 3){
+			return (
+				<div className={'row '+row.name}>{buttons.slice(row.start, row.end)}</div>
+			);	
+		} else{
+			// creates the last row with 2 columns
+			let buttonGroup;
+			const columns = row.columns.map(column => {
+				if(column.hasOwnProperty('end')){
+					buttonGroup = buttons.slice(column.start, column.end)	
+				} else{
+					buttonGroup = buttons.slice(column.start)	
+				}
+				return (
+					<div className={'inner-column '+column.name}>{buttonGroup}</div>
+				);
+			});
+			return (
+				<div className={'row '+row.name}>{columns}</div>
+			);
+		}
+	});
 	return(
+		// push rows to buttons element
 		<div id="buttons">
-			<div className="row one">
-				<Button id="AC" label="AC" type="wide" />
-				<Button id="divide" label="/" type="regular" />
-				<Button id="multiply" label="x" type="regular" />
-			</div>
-			<div className="row two">
-				<Button id="seven" label="7" type="regular" />
-				<Button id="eight" label="8" type="regular" />
-				<Button id="nine" label="9" type="regular" />
-				<Button id="subtract" label="-" type="regular" />
-			</div>
-			<div className="row three">
-				<Button id="four" label="4" type="regular" />
-				<Button id="five" label="5" type="regular" />
-				<Button id="six" label="6" type="regular" />
-				<Button id="add" label="+" type="regular" />
-			</div>
-			<div className="row four">
-				<div className="inner-column first">
-					<Button id="one" label="1" type="regular" />
-					<Button id="two" label="2" type="regular" />
-					<Button id="three" label="3" type="regular" />
-					<Button id="zero" label="0" type="wide" />
-					<Button id="decimal" label="." type="regular" />
-				</div>
-				<div className="inner-column second">
-					<Button id="equals" label="=" type="tall" />
-				</div>
-					
-			</div>
+			{rows}
 		</div>
 	)
 }
